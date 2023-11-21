dafaada94660eb7ea9ce26d5f549884d526ceec0
kimkihye <yunej170@gmail.com>
2023-11-21 16:30:33 +0900

    글꼴 2차 추가 수정

--- src/main/java/com/fitness/gymsup/Controller/NotifyBoardController.java
+++ src/main/java/com/fitness/gymsup/Controller/NotifyBoardController.java
@@ -1,14 +1,65 @@
 package com.fitness.gymsup.Controller;
 
+import com.fitness.gymsup.DTO.BoardDTO;
+import com.fitness.gymsup.Service.BoardService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
+@RequiredArgsConstructor
+@Log4j2
 public class NotifyBoardController {
+    private final BoardService boardService;
+
     @GetMapping("/board_list")
-    public String listAllForm(Model model) throws Exception {
+    public String listAllForm(@PageableDefault(page = 1) Pageable pageable,
+                              Model model) throws Exception {
+        Page<BoardDTO> boardDTOS = boardService.list(3, pageable);
+        int blockLimit = 5;
+        int startPage, endPage, prevPage, currentPage, nextPage, lastPage;
+
+        if(boardDTOS.isEmpty()) {
+            startPage = 0;
+            endPage = 0;
+            prevPage = 0;
+            currentPage = 0;
+            nextPage = 0;
+            lastPage = 0;
+        } else {
+            startPage = (((int)(Math.ceil((double) pageable.getPageNumber()/blockLimit)))-1) * blockLimit + 1;
+            //endPage = Math.min(startPage+blockLimit-1, boardDTOS.getTotalPages());
+            endPage = ((startPage+blockLimit-1)<boardDTOS.getTotalPages()) ? startPage+blockLimit-1 : boardDTOS.getTotalPages();
+
+            prevPage = boardDTOS.getNumber();
+            currentPage = boardDTOS.getNumber() + 1;
+            nextPage = boardDTOS.getNumber() + 2;
+            lastPage = boardDTOS.getTotalPages();
+        }
+
+        model.addAttribute("startPage", startPage);
+        model.addAttribute("endPage", endPage);
+        model.addAttribute("prevPage", prevPage);
+        model.addAttribute("currentPage", currentPage);
+        model.addAttribute("nextPage", nextPage);
+        model.addAttribute("lastPage", lastPage);
+
+        log.info("getTotalPages : " + boardDTOS.getTotalPages());
+        log.info("startPage : "+startPage);
+        log.info("endPage : "+endPage);
+        log.info("prevPage : "+prevPage);
+        log.info("currentPage : "+currentPage);
+        log.info("nextPage : "+nextPage);
+        log.info("lastPage : "+lastPage);
+
+        model.addAttribute("boardDTOS", boardDTOS);
+
         return "board/list";
     }
     @GetMapping("/board_notify_list")
