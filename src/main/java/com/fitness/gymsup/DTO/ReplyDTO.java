43c066dbd22126251d4a15bca6950c0267052785
JinnyBee <hjjeon81@gmail.com>
2023-11-23 14:45:52 +0900

    게시판 답글 목록조회/추가/삭제 기능 구현

---
+++ src/main/java/com/fitness/gymsup/DTO/ReplyDTO.java
@@ -0,0 +1,26 @@
+package com.fitness.gymsup.DTO;
+
+import lombok.*;
+
+import javax.validation.constraints.NotEmpty;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ReplyDTO {
+    private Integer id;                 //답글 id
+    private Integer commentId;          //원 댓글 id
+    private Integer boardId;            //원 게시글 id
+    private Integer userId;             //답글 작성자 id
+    private String userNickname;        //답글 작성자 닉네임
+
+    @NotEmpty(message = "답글을 적어주세요.")
+    private String  reply;              //답글 내용
+
+    private LocalDateTime regDate;      //생성일
+    private LocalDateTime modDate;      //수정일
+}
---
+++ src/main/java/com/fitness/gymsup/Service/ReplyService.java
@@ -0,0 +1,94 @@
+package com.fitness.gymsup.Service;
+
+import com.fitness.gymsup.DTO.CommentDTO;
+import com.fitness.gymsup.DTO.ReplyDTO;
+import com.fitness.gymsup.Entity.BoardEntity;
+import com.fitness.gymsup.Entity.CommentEntity;
+import com.fitness.gymsup.Entity.ReplyEntity;
+import com.fitness.gymsup.Entity.UserEntity;
+import com.fitness.gymsup.Repository.*;
+import com.fitness.gymsup.Util.FileUploader;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.management.relation.RoleInfo;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+@Log4j2
+public class ReplyService {
+    //주입 : Repository, ModelMapper, 파일업로드 클래스
+    private final ReplyRepository replyRepository;
+    private final UserRepository userRepository;
+    private final CommentRepository commentRepository;
+    private final ModelMapper modelMapper = new ModelMapper();
+
+    //답글 전체목록
+    public List<ReplyDTO> list(int commentId) throws Exception {
+        log.info("commentId : " + commentId);
+        List<ReplyEntity> replyEntities = replyRepository.findByCommentId(commentId);
+        List<ReplyDTO> replyDTOS = new ArrayList<>();
+
+        for(ReplyEntity replyEntity : replyEntities) {
+            ReplyDTO replyDTO = ReplyDTO.builder()
+                    .id(replyEntity.getId())
+                    .commentId(replyEntity.getCommentEntity().getId())
+                    .boardId(replyEntity.getCommentEntity().getBoardEntity().getId())
+                    .userId(replyEntity.getUserEntity().getId())
+                    .userNickname(replyEntity.getUserEntity().getNickname())
+                    .reply(replyEntity.getReply())
+                    .regDate(replyEntity.getRegDate())
+                    .modDate(replyEntity.getModDate())
+                    .build();
+            replyDTOS.add(replyDTO);
+        }
+
+        return replyDTOS;
+    }
+    //답글 등록
+    public void register(ReplyDTO replyDTO) throws Exception {
+        log.info("commentId : " + replyDTO.getCommentId());
+        //부모 댓글 Entity 조회
+        CommentEntity parentComment = commentRepository.findById(replyDTO.getCommentId()).orElseThrow();
+        //답글 작성자 Entity 조회
+        UserEntity writer = userRepository.findById(replyDTO.getUserId()).orElseThrow();
+
+        log.info(parentComment);
+        log.info(writer);
+
+        ReplyEntity replyEntity = modelMapper.map(replyDTO, ReplyEntity.class);
+        replyEntity.setCommentEntity(parentComment);
+        replyEntity.setUserEntity(writer);
+
+        replyRepository.save(replyEntity);
+    }
+    //답글 수정
+    public void modify(ReplyDTO replyDTO) throws Exception {
+        //답글 조회
+        ReplyEntity replyEntity = replyRepository.findById(replyDTO.getId()).orElseThrow();
+
+        //부모 댓글 Entity 조회
+        CommentEntity commentEntity = commentRepository.findById(replyDTO.getCommentId()).orElseThrow();
+
+        //답글 작성자 Entity 조회
+        UserEntity userEntity = userRepository.findById(replyDTO.getUserId()).orElseThrow();
+
+        ReplyEntity updateEntity = modelMapper.map(replyDTO, ReplyEntity.class);
+        updateEntity.setId(replyEntity.getId());
+        updateEntity.setCommentEntity(commentEntity);
+        updateEntity.setUserEntity(userEntity);
+
+        replyRepository.save(updateEntity);
+    }
+    //답글 삭제
+    public void remove(Integer id) throws Exception {
+        replyRepository.deleteById(id);
+    }
+}
