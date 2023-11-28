58aa99f47509c63bd120f7201e5cab2523fb8c50
orgcoli <alsgh9947@gmail.com>
2023-11-28 16:23:57 +0900

    운동기구 사용방법 작업중

--- src/main/java/com/fitness/gymsup/Repository/MachineUsageRepository.java
+++ src/main/java/com/fitness/gymsup/Repository/MachineUsageRepository.java
@@ -1,9 +1,14 @@
 package com.fitness.gymsup.Repository;
 
+import com.fitness.gymsup.Entity.MachineInfoEntity;
 import com.fitness.gymsup.Entity.MachineUsageEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface MachineUsageRepository extends JpaRepository<MachineUsageEntity, Integer> {
+
+    Page<MachineUsageEntity> findAllByMachineInfoEntity(Pageable pageable, MachineInfoEntity machineInfoEntity);
 }
