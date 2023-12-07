b09c8f8e62a232e249f0a3a0a0bb9d66bcbd8a0a
kimkihye <yunej170@gmail.com>
2023-12-07 11:15:28 +0900

    회의 후 결정할 이미지 푸시 및 gyminfoController 코드 정리

--- src/main/java/com/fitness/gymsup/Controller/GyminfoController.java
+++ src/main/java/com/fitness/gymsup/Controller/GyminfoController.java
@@ -11,48 +11,4 @@ public class GyminfoController {
     public String aboutForm(Model model) throws Exception {
         return "gyminfo/about";
     }
-    @GetMapping("/gyminfo_trainer")
-    public String trainerForm(Model model) throws Exception {
-        return "gyminfo/trainer";
-    }
-    @GetMapping("/gyminfo_program")
-    public String programForm(Model model) throws Exception {
-        return "gyminfo/program";
-    }
-    @GetMapping("/gyminfo_interior")
-    public String interiorForm(Model model) throws Exception {
-        return "gyminfo/interior";
-    }
-    @GetMapping("/gyminfo_location")
-    public String locationForm(Model model) throws Exception {
-        return "gyminfo/location";
-    }
-    @GetMapping("/gyminfo_list")
-    public String listForm(Model model) throws Exception {
-        return "gyminfo/list";
-    }
-    @GetMapping("/gyminfo_register")
-    public String registerForm(Model model) throws Exception {
-        return "gyminfo/register";
-    }
-    @PostMapping("/gyminfo_register")
-    public String registerProc(Model model) throws Exception {
-        return "redirect:/gyminfo_list";
-    }
-    @GetMapping("/gyminfo_detail")
-    public String detailForm(Model model) throws Exception {
-        return "gyminfo/detail";
-    }
-    @GetMapping("/gyminfo_modify")
-    public String modifyForm(Model model) throws Exception {
-        return "gyminfo/modify";
-    }
-    @PostMapping("/gyminfo_modify")
-    public String modifyProc(Model model) throws Exception {
-        return "redirect:/gyminfo_list";
-    }
-    @GetMapping("/gyminfo_remove")
-    public String removeProc(Model model) throws Exception {
-        return "gyminfo_remove";
-    }
 }
